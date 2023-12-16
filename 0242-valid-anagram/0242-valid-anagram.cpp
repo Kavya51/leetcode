1a60c7920cbdf9cce5c08fa2366b4ececda9bcdb
Kavya51 <121035380+Kavya51@users.noreply.github.com>
2023-12-16 10:53:52 +0530

    Time: 11 ms (45.06%), Space: 7.7 MB (79.21%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>m;
+        for(auto i:s)
+        m[i]++;
+        for(auto i:t)
+        m[i]--;
+        for(auto i:m)
+        {
+            if(i.second!=0)
+            return false;
+        }
+        return true;
+    }
+};
